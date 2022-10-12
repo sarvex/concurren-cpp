2f3ab3c8fd21f01764f079f2c7d60d3174835ca2
David Haim <59602013+David-Haim@users.noreply.github.com>
2022-10-12 11:11:38 +0300

    Update atomic_wait.cpp

--- source/threads/atomic_wait.cpp
+++ source/threads/atomic_wait.cpp
@@ -12,11 +12,11 @@
 
 namespace concurrencpp::details {
     void atomic_wait_native(void* atom, int32_t old) noexcept {
-        ::WaitOnAddress(atom, &old, sizeof(old), INFINITE);
+        ::WaitOnAddress(atom, &old, sizeof(atom), INFINITE);
     }
 
     void atomic_wait_for_native(void* atom, int32_t old, std::chrono::milliseconds ms) noexcept {
-        ::WaitOnAddress(atom, &old, sizeof(old), static_cast<DWORD>(ms.count()));
+        ::WaitOnAddress(atom, &old, sizeof(atom), static_cast<DWORD>(ms.count()));
     }
 
     void atomic_notify_one_native(void* atom) noexcept {
@@ -66,35 +66,172 @@ namespace concurrencpp::details {
     }
 }  // namespace concurrencpp::details
 
-#elif defined(CRCPP_MAC_OS)
+#else
 
-extern "C" {
-    int __ulock_wait(uint32_t op, void* addr, uint64_t value, uint32_t timeout);
-    int __ulock_wake(uint32_t op, void* addr, uint64_t wake_value);
-}  // extern "C"
+#include "concurrencpp/threads/cache_line.h"
 
-enum ulock_flags {
-	UL_COMPARE_AND_WAIT = 1,
-	ULF_WAKE_ALL = 0x00000100,
-	ULF_NO_ERRNO = 0x01000000
-};
+#    include <mutex>
+#    include <condition_variable>
+
+#    include <cstring>
+#    include <cassert>
 
 namespace concurrencpp::details {
+    class atomic_wait_table {
+
+       private:
+        constexpr static size_t k_wait_table_size = 257;
+
+        struct wait_context {
+            const void* const storage_ptr;
+            wait_context* next = nullptr;
+            wait_context* prev = nullptr;
+            std::condition_variable cv;
+
+            wait_context(const void* storage_ptr) : storage_ptr(storage_ptr) {}
+        };
+
+        struct alignas(CRCPP_CACHE_LINE_ALIGNMENT) wait_table_bucket {
+            std::mutex lock;
+            wait_context* head = nullptr;
+
+            void push_front_wait_ctx(wait_context* ctx) noexcept {
+                assert(ctx != nullptr);
+                assert(ctx->next == nullptr);
+                assert(ctx->prev == nullptr);
+
+                ctx->next = head;
+
+                if (head != nullptr) {
+                    head->prev = ctx;
+                }
+
+                head = ctx;
+            }
+
+            void remove_wait_ctx(wait_context* ctx) {
+                assert(ctx != nullptr);
+                assert(head != nullptr);
+
+                if (head == ctx) {
+                    head = ctx->next;
+                }
+
+                if (ctx->next != nullptr) {
+                    ctx->next->prev = ctx->prev;
+                }
+
+                if (ctx->prev != nullptr) {
+                    ctx->prev->next = ctx->next;
+                }
+            }
+        };
+
+        struct scoped_wait_context : public wait_context {
+            wait_table_bucket& parent_bucket;
+
+            scoped_wait_context(wait_table_bucket& parent_bucket, const void* storage_ptr) :
+                wait_context(storage_ptr), parent_bucket(parent_bucket) {
+                parent_bucket.push_front_wait_ctx(this);
+            }
+
+            ~scoped_wait_context() noexcept {
+                parent_bucket.remove_wait_ctx(this);
+            }
+        };
+
+       private:
+        wait_table_bucket m_wait_table[k_wait_table_size];
+
+        wait_table_bucket& get_bucket_for(const void* const storage_ptr) noexcept {
+            const std::hash<std::uintptr_t> hasher;
+            const auto address = reinterpret_cast<std::uintptr_t>(storage_ptr);
+            const auto index = hasher(address) % k_wait_table_size;
+            return m_wait_table[index];
+        }
+
+       public:
+        void notify_one(const void* const storage_ptr) noexcept {
+            auto& bucket = get_bucket_for(storage_ptr);
+
+            std::unique_lock<std::mutex> lock(bucket.lock);
+            for (auto cursor = bucket.head; cursor != nullptr; cursor = cursor->next) {
+                if (cursor->storage_ptr == storage_ptr) {
+                    cursor->cv.notify_all();
+                    break;
+                }
+            }
+        }
+
+        void notify_all(const void* const storage_ptr) noexcept {
+            auto& bucket = get_bucket_for(storage_ptr);
+
+            std::unique_lock<std::mutex> guard(bucket.lock);
+            for (auto cursor = bucket.head; cursor != nullptr; cursor = cursor->next) {
+                if (cursor->storage_ptr == storage_ptr) {
+                    cursor->cv.notify_all();
+                }
+            }
+        }
+
+        void wait(const void* const storage_ptr, const void* const comparand, size_t size) noexcept {
+            auto& bucket = get_bucket_for(storage_ptr);
+
+            std::unique_lock<std::mutex> lock(bucket.lock);
+            scoped_wait_context context(bucket, storage_ptr);
+
+            while (true) {
+                if (std::memcmp(storage_ptr, comparand, size) != 0) {
+                    return;
+                }
+
+                context.cv.wait(lock);
+            }
+        }
+
+        bool wait(const void* const storage_ptr,
+                  const void* const comparand,
+                  size_t size,
+                  std::chrono::milliseconds timeout_ms) noexcept {
+            const auto deadline = std::chrono::system_clock::now() + timeout_ms;
+            auto& bucket = get_bucket_for(storage_ptr);
+
+            std::unique_lock<std::mutex> lock(bucket.lock);
+            scoped_wait_context context(bucket, storage_ptr);
+
+            while (true) {
+                if (std::memcmp(storage_ptr, comparand, size) != 0) {
+                    return true;
+                }
+
+                if (std::chrono::system_clock::now() >= deadline) {
+                    return false;
+                }
+
+                context.cv.wait_until(lock, deadline);
+            }
+        }
+
+        static atomic_wait_table& instance() {
+            static atomic_wait_table s_atomic_wait_table;
+            return s_atomic_wait_table;
+        }
+    };
+
     void atomic_wait_native(void* atom, int32_t old) noexcept {
-        __ulock_wait(UL_COMPARE_AND_WAIT | ULF_NO_ERRNO, atom, old, 0);
+        atomic_wait_table::instance().wait(atom, &old, sizeof(old));
     }
 
     void atomic_wait_for_native(void* atom, int32_t old, std::chrono::milliseconds ms) noexcept {
-        const auto us = std::chrono::duration_cast<std::chrono::microseconds>(ms);
-        __ulock_wait(UL_COMPARE_AND_WAIT | ULF_NO_ERRNO, atom, old, us.count());
+        atomic_wait_table::instance().wait(atom, &old, sizeof(old), ms);
     }
 
     void atomic_notify_one_native(void* atom) noexcept {
-        __ulock_wake(UL_COMPARE_AND_WAIT | ULF_NO_ERRNO, atom, 0);
+        atomic_wait_table::instance().notify_one(atom);
     }
 
     void atomic_notify_all_native(void* atom) noexcept {
-        __ulock_wake(UL_COMPARE_AND_WAIT | ULF_WAKE_ALL | ULF_NO_ERRNO, atom, 0);
+        atomic_wait_table::instance().notify_all(atom);
     }
 }  // namespace concurrencpp::details
 
