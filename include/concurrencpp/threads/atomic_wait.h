c0c00023867bd3edd995df193e634cccecf1aa65
David-Haim <dhayim1@gmail.com>
2022-12-17 00:14:52 +0200

    add CRCPP_API to atomic_wait

--- include/concurrencpp/threads/atomic_wait.h
+++ include/concurrencpp/threads/atomic_wait.h
@@ -7,15 +7,15 @@
 #include <cassert>
 
 namespace concurrencpp::details {
-    void atomic_wait_native(void* atom, int32_t old) noexcept;
-    void atomic_wait_for_native(void* atom, int32_t old, std::chrono::milliseconds ms) noexcept;
-    void atomic_notify_one_native(void* atom) noexcept;
-    void atomic_notify_all_native(void* atom) noexcept;
+    void CRCPP_API atomic_wait_native(void* atom, int32_t old) noexcept;
+    void CRCPP_API atomic_wait_for_native(void* atom, int32_t old, std::chrono::milliseconds ms) noexcept;
+    void CRCPP_API atomic_notify_one_native(void* atom) noexcept;
+    void CRCPP_API atomic_notify_all_native(void* atom) noexcept;
 
     enum class atomic_wait_status { ok, timeout };
 
     template<class type>
-    void atomic_wait(std::atomic<type>& atom, type old, std::memory_order order) noexcept {
+    void CRCPP_API atomic_wait(std::atomic<type>& atom, type old, std::memory_order order) noexcept {
         static_assert(std::is_standard_layout_v<std::atomic<type>>, "atomic_wait - std::atom<type> is not standard-layout");
         static_assert(sizeof(type) == sizeof(int32_t), "atomic_wait - <<type>> must be 4 bytes.");
 
@@ -30,7 +30,7 @@ namespace concurrencpp::details {
     }
 
     template<class type>
-    atomic_wait_status atomic_wait_for(std::atomic<type>& atom,
+    atomic_wait_status CRCPP_API atomic_wait_for(std::atomic<type>& atom,
                                        type old,
                                        std::chrono::milliseconds ms,
                                        std::memory_order order) noexcept {
@@ -61,12 +61,12 @@ namespace concurrencpp::details {
     }
 
     template<class type>
-    void atomic_notify_one(std::atomic<type>& atom) noexcept {
+    void CRCPP_API atomic_notify_one(std::atomic<type>& atom) noexcept {
         atomic_notify_one_native(&atom);
     }
 
     template<class type>
-    void atomic_notify_all(std::atomic<type>& atom) noexcept {
+    void CRCPP_API atomic_notify_all(std::atomic<type>& atom) noexcept {
         atomic_notify_all_native(&atom);
     }
 }  // namespace concurrencpp::details
